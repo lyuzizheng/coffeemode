549dee30f1612ddebee7c022e0d7870d9c5e41f1
ZizhengLezzoo <lyu.zizheng@lezzoo.com>
2026-08-29 10:40:35 +0800

    fix(poi-service): import MAX_EXTERNAL_BATCH_SIZE from shared places constants (Fixes #273)

--- poi-service/src/constants.ts
+++ poi-service/src/constants.ts
@@ -2,16 +2,14 @@
 
 import {
   DEFAULT_SEARCH_RADIUS_KM,
+  MAX_EXTERNAL_BATCH_SIZE,
   MAX_SEARCH_RADIUS_KM,
 } from "../../web/shared/places/constants";
 
-export { DEFAULT_SEARCH_RADIUS_KM, MAX_SEARCH_RADIUS_KM };
+export { DEFAULT_SEARCH_RADIUS_KM, MAX_EXTERNAL_BATCH_SIZE, MAX_SEARCH_RADIUS_KM };
 
 /** Maximum number of rows /poi/search will ever return. */
 export const SEARCH_RESULT_LIMIT = 100;
-
-/** Maximum entries accepted in one POST /poi/external request. */
-export const MAX_EXTERNAL_BATCH_SIZE = 100;
 
 /** KV hot cache TTL and the D1 "fresh" window are the same by design:
  *  a row younger than this is served without hitting Google. */
